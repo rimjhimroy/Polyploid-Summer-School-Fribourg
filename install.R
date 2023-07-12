e6c0e37ef52ab9326f6ef5403a4cb603e7fa2727
Rimjhim Roy Choudhury <rimjhim.roy.ch@gmail.com>
2023-07-12 20:30:10 +0000

    updated dependencies

--- install.R
+++ install.R
@@ -1,3 +1,12 @@
 install.packages("BiocManager")
+BiocManager::install(c("Biostrings","VariantAnnotation","GenomicRanges","Rsamtools","rtracklayer","S4Vectors","edgeR"))
 install.packages("updog")
+install.packages("data.table")
+install.packages("tidyverse")
+install.packages("optparse")
+install.packages("gridExtra")
+install.packages("purrr")
+install.packages(c("car","agricolae","lme4","VennDiagram"))
+
+
 
